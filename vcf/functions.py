285f3758cab6bfb8066097914eda48de790e50df
Axel Jensen <axeljensen@AirsomtllhorOSX.bredband2.com>
2022-08-03 20:13:44 +0200

    Added script to run window phylogenies with iqtree or phyml

--- vcf/functions.py
+++ vcf/functions.py
@@ -102,3 +102,122 @@ def Diploidize(sample_alleles):
 		seqDict[sample + "__2"] = sample_alleles[sample][1]
 	return seqDict
 
+def getChromLengths(vcf, only_contigs_with_records=True):
+	all_contigs = {contig: 0 for contig in list(vcf.header.contigs)}
+	# fetch all the chromosomes to begin with from the vcf header
+	for contig in all_contigs.keys():
+		length = vcf.header.contigs[contig].length
+		all_contigs[contig] = length
+	contigs = {}
+	if only_contigs_with_records:
+		# usually we'll only want the chromosome lengths for the chromosome(s) that has records in this vcf file
+		for contig,length in all_contigs.items():
+			try: 
+				# check this by trying to fetch the first record on each chromosome, if successfull, add this contig to the returned dict
+				next(vcf.fetch(contig))
+				contigs[contig] = length
+			except:
+				pass
+	else:
+		contigs = all_contigs 
+	return contigs
+
+#split a dictionary with genomic intervals into window of "window" size, separated by step_size
+def generateWindows(intervals, window, step_size):
+	# first, if the intervals are given as a simle chrom: length dictionary, add 0 as start position to each such interval
+	if not type(intervals[list(intervals.keys())[0]]) is tuple:
+		for c in intervals.keys():
+			intervals[c] = (0, intervals[c])
+    #create a dictionary with windows for each chromosome represented in intervals
+	windows = []
+	window_number = 1
+	end = window
+	for c in intervals.keys():
+		start = intervals[c][0]
+		length = intervals[c][1]
+		chrom = c
+		while start < length:
+			windows.append({'window_number': window_number, 'chrom': chrom, 'start':start, 'end':end})
+			start = start + step_size
+			end = start + window if start + window < length else length
+			window_number = window_number + 1
+		else:
+			break
+	return windows
+
+# function to parse a vcf file/region to a sequence dictionary
+def vcfToSeqDict(vcf, chrom, haploidize = True, handle_heterozygotes = "IUPAC", samples=None, start=None, end=None):
+	if not samples:
+		samples = list(vcf.header.samples)
+	if haploidize:
+		seqDict = {s: [] for s in samples}
+	else:
+		seqDict = {}
+		for s in samples:
+			seqDict[s + "__1"] = []
+			seqDict[s + "__2"] = []
+	for rec in vcf.fetch(chrom,start,end):
+		siteDict = {s: [] for s in seqDict.keys()}
+		alleles = {0: rec.ref}
+		c = 1
+		for a in range(0,len(rec.alts)):
+			alleles[c] = rec.alts[a]
+			c +=1
+		alleles['missing'] = "N"
+		# add dashes to the alleles to match up the longest allele, in case it's an indel or mixed
+		longest_allele = 0
+		for a in alleles.values():
+			if len(a) > longest_allele:
+				longest_allele = len(a)
+		if longest_allele > 1:
+			print("Warning: found alleles longer then 1 bp (i.e. not only biallelic SNPs). This could lead to problems in the output alignment, as alleles of differing lengths may come out as non-aligned.")
+		for k,a in alleles.items():
+			allele = a + "-" * (longest_allele - 1 - len(a))
+		for s in samples:
+			gt_int = rec.samples[s]['GT']
+			if None in gt_int:
+				gt_dip = (alleles['missing'], alleles['missing'])
+			else:
+				gt_dip = (alleles[gt_int[0]], alleles[gt_int[1]])
+			if haploidize:
+				if len(set(gt_dip)) > 1:
+					if handle_heterozygotes == "IUPAC":
+						gt_hap = IUPAC(gt_dip)
+					elif handle_heterozygotes == "random":
+						gt_hap = gt_dip[random.randint(0,1)]
+				else:
+					gt_hap = gt_dip[0]
+				siteDict[s].append(gt_hap)
+			else:
+				siteDict[s + "__1"] = [gt_dip[0]]
+				siteDict[s + "__2"] = [gt_dip[1]]
+		seqDict = {k: v + siteDict[k] for k,v in seqDict.items()}
+	# last we join the list together as strings, and do a sanity check confirming all sequences are of the same lengths
+	lengths = []
+	for k,v in seqDict.items():
+		seqDict[k] = ''.join(v)
+		lengths.append(len(v))
+	if len(set(lengths)) > 1:
+		print("The sequences came out with differing lengths for some reason, which is not ok. Exiting.")
+		sys.exit()
+	return seqDict
+
+def filterSeqDict(seqDict, maxmissing):
+	lengths = [len(seqDict[k]) for k in seqDict.keys()]
+	if len(set(lengths)) > 1:
+		print("Differing lengths found in this alignment, exiting..")
+		sys.exit()
+	length = lengths[0]
+	missing = {i: 0 for i in range(0, length)}
+	for i in range(0,length):
+		for s in seqDict.keys():
+			if seqDict[s][i] in ['N','n','-']:
+				missing[i] += 1
+		missing[i] = missing[i] / len(list(seqDict.keys()))
+	filtered_seqDict = {s: "" for s in seqDict.keys()}
+	for pos,missing in missing.items():
+		if missing <= maxmissing:
+			for s in seqDict.keys():
+				filtered_seqDict[s] = filtered_seqDict[s] + seqDict[s][pos]
+	return filtered_seqDict
+
---
+++ window_phylogenies/windowPhylogenies.py
@@ -0,0 +1,420 @@
+#!/usr/bin/python
+import sys
+import os
+dir_path = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, dir_path)
+import argparse
+import gzip
+from multiprocessing import Process
+from multiprocessing import SimpleQueue
+from threading import Thread
+from time import sleep
+import tempfile
+from zipfile import ZipFile
+from pysam import VariantFile
+import random
+from vcf.functions import getChromLengths, vcfToSeqDict, generateWindows, filterSeqDict
+import seqtools.functions as seqtools
+from datetime import datetime
+import glob
+
+#function that reads a vcf file and outputs an alignment given specified coordinates
+def SplitVcfInWindows(input_vcf, samples, windows, haploidize, max_missing=None):
+    global windowsQueued
+    global resultsReceived
+    #next start generating windows
+    for w in windows:
+        seqDict = vcfToSeqDict(vcf, w['chrom'], haploidize = haploidize, handle_heterozygotes = handle_heterozygotes, samples=samples, start=w['start'], end=w['end'])
+        if max_missing:
+            seqDict = filterSeqDict(seqDict, maxmissing)
+        filteredLength = len(list(seqDict.values())[0])
+        windowsQueued +=1
+        windowQueue.put((w, seqDict))
+
+def IQTreeCommand(iqtree_path, model, bootstraps, alignment, outgroup, prefix, logfile, bootstrap_flag):
+	if outgroup:
+		outgroupArg = " -o " + outgroup
+	else:
+		outgroupArg = ""
+	if bootstraps:
+		bootstraps_command = " -B " + str(bootstraps)
+	else:
+		bootstraps_command = ""
+	iqtree_command = iqtree_path + "2" + " -s " + alignment + " -pre " + prefix + outgroupArg + bootstraps_command + " -m " + model + " -nt 1 " + ">>" + logfile + " 2>&1 "
+	os.system(iqtree_command)
+
+def PHYMLCommand(phyml_path, model, opt, bootstraps, alignment, directory, prefix, logfile):
+	if opt:
+		opt_command = " -o " + opt
+	else:
+		opt_command = " -o n "
+	if bootstraps:
+		bootstraps_command = " -b " + str(bootstraps)
+	else:
+		bootstraps_command = " -b 0 "
+	phyml_command = phyml_path + " -i " + alignment + bootstraps_command + opt_command + " --model " + model + ">>" + logfile + " 2>&1 "
+	os.system(phyml_command)
+
+def parseIQTREEOutput(directory, prefix, windowCoords):
+	try:
+		tree_logfile = open(os.path.join(prefix + ".log"))
+		for line in tree_logfile.readlines():
+			if "Alignment has" in line:
+				info_line = line
+		sequences = info_line.split()[2]
+		alignment_sites = info_line.split()[5]
+		treefile = open(os.path.join(prefix + ".treefile"))
+		tree = treefile.readline().rstrip('\n')
+		results = True
+	except:
+		sys.stderr.write("Warning: no tree was constructed for window {chrom}:{start}-{end}.\n".format(chrom=windowCoords['chrom'], start=windowCoords['start'], end=windowCoords['end']))
+		tree = "NA"
+		results = False
+		alignment_sites = "NA"
+	return tree,alignment_sites
+
+def parsePHYMLOutput(prefix, windowCoords):
+	try:
+		with open(prefix + "_phyml_tree.txt", "rt") as treeFile: 
+			tree = treeFile.readline().strip()
+			results = True
+	except:
+		try:
+			with open(prefix + "_phyml_tree", "rt") as treeFile:
+				tree = treeFile.readline().strip()
+				results = True
+		except:
+			sys.stderr.write("Tree not found at " + prefix + "_phyml_tree.txt\n")
+			tree = "NA"
+			results = False
+			sys.stderr.write("Warning: no tree was constructed for window {chrom}:{start}-{end}.\n".format(chrom=windowCoords['chrom'], start=windowCoords['start'], end=windowCoords['end']))
+	try:
+		with open(prefix + "_phyml_stats.txt", "rt") as statsFile:
+			stats = statsFile.read().split()
+			lnL = stats[stats.index("Log-likelihood:")+1]
+	except:
+		try:
+			with open(prefix + "_phyml_stats", "rt") as statsFile:
+				stats = statsFile.read().split()
+				lnL = stats[stats.index("Log-likelihood:")+1]
+		except:
+			lnL = "NA"
+			stats = "NA"
+	return tree,stats,lnL
+
+def run_window_phylogeny(windowQueue, software, model, bootstraps, opt, outgroup, directory, output_temp, logfile, bootstrap_flag, phyml_path=None, iqtree_path=None, test=False):
+	global resultsReceived
+	global timePassed
+	global windowsTotal
+	while True:
+		if _FINISH:
+			break
+		windowCoords, seqDict = windowQueue.get()
+		seqLen = len(seqDict[list(seqDict.keys())[0]])
+		nseq = len(list(seqDict.keys()))
+		prefix = os.path.join(os.path.basename(directory) + "_" + str(windowCoords['start']) + "_" + str(windowCoords['end']))
+		tempAlignment = tempfile.NamedTemporaryFile(mode="w", prefix=prefix, suffix=".phy", dir = directory, delete=False)
+		seqtools.WriteSeqDictToPhylip(seqDict, tempAlignment.name)
+		outprefix = os.path.join(directory, prefix)
+		tempAlignment.close()
+		if software == "phyml":
+			PHYMLCommand(phyml_path, model, opt, bootstraps, tempAlignment.name, directory, outprefix, logfile)
+			tree,stats,lnL = parsePHYMLOutput(outprefix, windowCoords)
+			treeDict = {'window_number':str(windowCoords['window_number']), 'chrom': str(windowCoords['chrom']), 'start':str(int(windowCoords['start']) + 1), 'stop':str(windowCoords['end']), 'lnL': str(lnL), 'samples': str(nseq), 'sites':str(seqLen), 'tree':tree}
+		elif software == "iqtree":
+			IQTreeCommand(iqtree_path, model, bootstraps, tempAlignment.name, outgroup, outprefix, logfile, bootstrap_flag)
+			tree,nsites = parseIQTREEOutput(directory, outprefix, windowCoords)
+			treeDict = {'window_number':str(windowCoords['window_number']), 'chrom': str(windowCoords['chrom']), 'start':str(int(windowCoords['start']) + 1), 'stop':str(windowCoords['end']), 'sequences':str(nseq), 'sites':str(nsites), 'tree':tree}
+		open(output_temp, "a").write(str('\t'.join(treeDict.values())) + "\n")
+		resultsReceived +=1
+		# remove all files from this run
+		if not test:
+			filelist = glob.glob(os.path.join(directory, prefix) + "*")
+			for f in filelist:
+				os.remove(f)
+			
+		#print("Finished with " + str(resultsReceived) + "/" + str(windowsTotal) + " windows.")
+
+##watch result queue and send sorted results to writeQueue
+def sort_results(resultQueue, writeQueue):
+    global resultsReceived
+    global resultsHandled
+    global windowsQueued
+    results_dictlist = []
+    while True:
+        if _FINISH:
+            break
+        while windowsQueued > resultsReceived:
+            treeDict = resultQueue.get()
+            writeQueue.put(treeDict)
+            resultsReceived +=1
+
+#function to grab trees from writing queue and write to outputfile
+def write_results(writeQueue, output):
+    resultBuffer = []
+    while True:
+        if _FINISH:
+            break
+        resultOut = writeQueue.get()
+        #store results in dictionary and write in sorted order when all results are in
+        resultBuffer.append(resultOut)
+        output_temp.write(str('window_number') + "\t" + str(resultOut['start']) + "\t" + str(resultOut['stop']) + "\t" + str(resultOut['sequences']) + "\t" +
+        str(resultOut['sites']) + "\t" + str(resultOut['tree']) + "\n")
+        resultsWritten +=1
+        print("Retrieved result from window " + str(str(resultOut['window_number'])))
+        if not resultsWritten == 0 and resultsWritten == windowsQueued:
+            break
+##indefinite loop to write some stats every tenth second as the program runs:
+def checkStats():
+	global resultsReceived
+	time_passed = 0
+	while True:
+		sleep(30)
+		time_passed = time_passed + 30
+		percentage = (resultsReceived / windowsTotal) * 100
+		sys.stderr.write("{time}: Received results for {received}/{total} windows ({perc} %)\n".format(time = datetime.now().strftime("%H:%M:%S"), received = resultsReceived, total = windowsTotal, perc=round(percentage, 2)))
+		#print("Finished with " + str(resultsReceived) + "/" + str(windowsTotal) + " windows (" + str(percentage) + "%) in " + str(time_passed) + " seconds.")
+
+#just a function to check whether to use gzip or not when opening input
+def openFile(filepath, mode='r'):
+	if filepath.endswith(".gz"):
+		return gzip.open(filepath, 'rt')
+	else:
+		return open(filepath, mode)
+    
+##########################################################################################################################
+
+#if __name__ == "__main__":
+
+parser = argparse.ArgumentParser()
+
+#arguments for in and output:
+parser.add_argument("-i", "--input", help="Input vcf file (.vcf or .vcf.gz).", required=True, metavar="snps.vcf | spns.vcf.gz")
+parser.add_argument("-o", "--output", help="Output file.", required=True, metavar="output.tsv")
+
+#arguments for subsetting and splitting alignment:
+parser.add_argument("--sequences", help="Headers of sequences to include, either as a file with one header per line, or in the command separated by commas.", metavar="<seq1,seq2,seq3>|seqs.file")
+parser.add_argument("--max-missing", type=int, help="Maximum number of missing bases ('N' or '-') allowed for a posititon to be kept.", metavar="<int:nBases>")
+
+parser.add_argument("-w", "--window-size", help="Window size in bp.", type=int, metavar="bases")
+parser.add_argument("--step-size", help="Step size in bp.", type=int, metavar="bases")
+
+parser.add_argument("--haploidize", help="Combine/subsample diploid loci to one haploid sequence", action="store_true")
+parser.add_argument("--handle-heterozygotes", help="Either randomly sample an allele (random) or use IUPAC codes (IUPAC)", type=str, default="IUPAC")
+
+#arguments for running iqtree:
+parser.add_argument("--iqtree-path", help="If iqtree executable isn't in PATH, you can specify the absolute path here", metavar="path/to/iqtree", default="iqtree")
+parser.add_argument("--model", help="iqtree substitution model", default="GTR")
+parser.add_argument("--outgroup", help="Outgroup(s) for the tree construction, if desired", default=None, metavar="outgroup_seq1,outgroup_seq2")
+parser.add_argument("--bootstraps", help="Number of ultrarapid bootstraps to run in iqtree, defaults to none.", type=int, metavar="number_of_bootstraps")
+
+#number of threads we're running on:
+parser.add_argument("-T", "--threads", help="Number of threads for parallel processing", type=int, default=1, metavar="threads")
+
+#can give a file with predefined coordinates/windows to build trees on
+parser.add_argument("-R", "--regions", help="File with predefined windows/regions to analyze. 'chrom\tstart\tstop'. Only works when input is a vcf as of now.")
+
+#optionally run phyml instead of iqtree, which is still the default for now
+parser.add_argument("--phyml", help="Specify to run phyml instead of iqtree.", action="store_true")
+parser.add_argument("--phyml-path", type=str, help="Path to phyml executable, if not in path", default="phyml")
+#a few options specific to phyml
+parser.add_argument("--phyml-model", type=str, default="HKY85", help="Substitution model for running phyml.")
+parser.add_argument("--optimize", type=str, default="n", help="Parameter optimization, see phyml manual for options.")
+
+# an option to run analyses in a separate directory from wd
+parser.add_argument("--analysis-directory", type=str, default=".")
+
+parser.add_argument("--test", help="a bit more verbose output for debugging.", action="store_true")
+args = parser.parse_args()
+
+# print date etc
+sys.stderr.write("#" * 100 + "\n\n" + "{datetime}: starting window phylogenies analyses.\n\n".format(datetime = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")) + "#" * 100 + "\n\n")
+
+### open files
+vcf = VariantFile(args.input)
+output_file = open(args.output, "w")
+
+#set step length same as window if not given:
+if args.step_size:
+	step_size=args.step_size
+else:
+	step_size=args.window_size
+sys.stderr.write("Parameters: \n")
+sys.stderr.write("Input vcf read from: {inputfile}, writing final window output to {outputfile}.\n".format(inputfile=args.input, outputfile=args.output))
+sys.stderr.write("Window size: {wsize}, step size: {stepsize}\n".format(wsize=str(args.window_size), stepsize=str(step_size)))
+#if regions file is provided, parse this one to a dictionary
+if args.regions:
+	regions = []
+	with open(args.regions) as r:
+		for number,line in enumerate(r.readlines()):
+			if not line == "":
+				regions.append({'window':number, 'chrom':line.split()[0], 'start':line.split()[1], 'stop':line.split()[2]})
+	sys.stderr.write("Intervals were supplied from file, using these intervals.\n")			
+# otherwise chop up the genome
+else:
+	sys.stderr.write("No interval file was supplied, will generate windows from the contigs in the vcf input.\n")
+	chroms = getChromLengths(vcf)
+	regions = generateWindows(chroms, args.window_size, step_size)
+windowsTotal = len(regions)
+if not args.bootstraps:
+	bootstraps = None
+else:
+	bootstraps = args.bootstraps
+# set bootstrap flag to "-bb" as default
+bootstrap_flag = "-bb" 
+
+#create a temporary directory to work in
+analysis_directory = args.analysis_directory
+tmpdir = tempfile.mkdtemp(suffix=None, prefix=None, dir=analysis_directory)
+
+###create tempfile to store unsorted trees in:
+output_temp = os.path.join(tmpdir, os.path.basename(args.input) + "_out_temp")
+
+#write headers to output file and check that the software needed is there
+if not args.phyml:
+	if args.iqtree_path:
+		iqtree = args.iqtree_path
+	else:
+		iqtree = "iqtree"
+	t = os.system(iqtree + " >> /dev/null 2>&1")
+	if t > 0:
+		t = os.system("iqtree2  >> /dev/null 2>&1")
+		if t > 0:
+			sys.stderr.write("Error: Please make sure you have iqtree or iqtree2 in your PATH, or give full path to executable. Will exit now.\n")
+			sys.exit(1)
+		else:
+			sys.stderr.write("Running iqtree2.\n")
+			iqtree = "iqtree2"
+			bootstrap_flag = "-B"
+	logfile = args.output[:-4] + "_iqtree.log.txt"
+	sys.stderr.write("Running phylogenies with IQTree.\n")
+	output_file.write("window\tchrom\tstart\tend\tsequences\tsites\ttree\n")
+else:
+	# check so that phyml is in path
+	if args.phyml_path:
+		phyml = args.phyml_path
+	else:
+		phyml = "phyml"
+	t = os.system("phyml >> /dev/null 2>&1")
+	if t > 0:
+		sys.stderr.write("Error: Please make sure you have phyml in your PATH, or give full path to executable. Will exit now.\n")
+		sys.exit(1)
+	logfile = args.output[:-4] + "_phyml.log.txt"
+	sys.stderr.write("Running phylogenies with PHYML.\n")
+	output_file.write("window\tchrom\tstart\tend\tLnl\tsequences\tsites\ttree\n")
+
+if os.path.isfile(logfile):
+	sys.stderr.write("Logfile with the given prefix already exist, this will be overwritten.\n")
+	os.remove(logfile)
+
+#If sequences to subsample are given to input, extract those:
+if args.sequences:
+	if os.path.isfile(args.sequences):
+		headers_to_keep = open(args.sequences).read().split("\n")
+		headers_to_keep = [sample for sample in headers_to_keep if not sample == ""]
+	else:
+		headers_to_keep = args.sequences
+	sys.stderr.write("Keeping the specified samples for analysis:\n " + ','.join([sample for sample in headers_to_keep if not sample == ""]) + "\n")
+else:
+	headers_to_keep = "all"
+	sys.stderr.write("Keeping all samples in the vcf file for analyses.\n") 
+#outgroup to none if not given
+if not args.outgroup:
+	outgroup=None
+	sys.stderr.write("No outgroup was specified.\n") 
+else:
+	outgroup=args.outgroup
+	sys.stderr.write("Using the following sample(s) as outgroup: {}.\n".format(outgroup)) 
+
+# are we running phyml or iqtree?
+if args.phyml:
+	model = args.phyml_model
+	software = "phyml"
+	opt = args.optimize
+	sys.stderr.write("Will run PHYML using the {phymlmodel} model, and optimization set to {optim}\n".format(phymlmodel = model, optim = opt))
+else:
+	software = "iqtree"
+	model = args.model
+	opt = None
+	sys.stderr.write("Will run IQTree using the {iqtreemodel} model.\n".format(iqtreemodel = model))
+sys.stderr.write("Bootstraps set to {}\n".format(bootstraps))
+
+# should we haploidize the sequences?
+if args.haploidize:
+	haploidize = True
+	handle_heterozygotes = args.handle_heterozygotes
+	sys.stderr.write("Heterozygous positions will be haploidized, and replacing heterozygous sites with a {} base.\n".format(handle_heterozygotes))
+else:
+	haploidize = False
+	handle_heterozygotes = args.handle_heterozygotes
+	sys.stderr.write("Will generate a diploid sequence for each samples, suffixed __1 and __2. Phasing will not be taken into account here.\n")
+
+#### Global variables for keeping track of stats:
+global _FINISH
+_FINISH = False
+global windowsQueued
+windowsQueued = 0
+global resultsReceived
+resultsReceived = 0
+resultsWritten = 0
+resultsHandled = 0
+windowsTotal = windowsTotal
+global analysisStarted
+### Create queues
+#queue for holding alignment windows:
+windowQueue = SimpleQueue()
+#queue for holding results:
+resultQueue = SimpleQueue()
+#que for holding sorted results:
+writeQueue = SimpleQueue()
+
+
+#list to put all processes in:
+workerprocesses = []
+for thread in range(args.threads):
+	worker = Thread(target=run_window_phylogeny, args=(windowQueue, software, model, 
+	bootstraps, opt, outgroup, tmpdir, output_temp, logfile, bootstrap_flag, args.phyml_path, 
+	args.iqtree_path))
+	worker.daemon = True
+	worker.start()
+analysisStarted = True
+stats = Thread(target=checkStats)
+stats.daemon = True
+stats.start()
+sys.stderr.write("\n" + "#" * 100 + "\n\n")
+sys.stderr.write("Starting analysis on " + str(args.threads) + " processes in parallell.\n")
+
+# start generating the window alignments and trees
+if headers_to_keep == "all":
+	samples = list(vcf.header.samples)
+else:
+	samples = headers_to_keep
+
+SplitVcfInWindows(args.input, samples, regions, haploidize, max_missing=args.max_missing)
+analysisStarted = True
+while resultsReceived < windowsQueued:
+	sleep(10)
+#	print("Received: " + str(resultsReceived) + ", queued: " + str(windowsQueued))
+print("Done.")
+
+_FINISH=True
+
+sys.stderr.write("\n\nDone with analysis, sorting and writing final output.\n")
+#grab unsorted outtemp, loop through it to sort outfiles
+results_unsorted = []
+with open(output_temp) as file:
+	for line in file.readlines():
+		results_unsorted.append(line)
+for i in range(1, len(results_unsorted) + 1):
+	for result in results_unsorted:
+		if result.split()[0] == str(i):
+			output_file.write(result)
+output_file.close()
+if not args.test:
+	for i in os.listdir(tmpdir):
+		os.remove(os.path.join(tmpdir, i))
+	os.rmdir(tmpdir)
+#	os.remove(output_temp)
+sys.stderr.write("All done.\n")
+sys.exit(0)
+
